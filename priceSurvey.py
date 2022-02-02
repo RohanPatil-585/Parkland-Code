832761cfcdf08da409485003ee87160de71fe71b
rohanPatil585 <rohan.patil@driverdo.com>
2022-02-02 12:12:39 +0530

    first comit - added old project to new account

---
+++ priceSurvey.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import json
+import boto3
+# boto3 S3 initialization
+# third lambda function
+# test price survey
+# test1
+s3_client = boto3.client("s3")
+def lambda_handler(event, context):
+    for record in event['Records']:
+        print("test")
+        payload = record["body"]
+        record_dict = json.loads(payload)
+        s3_client.copy_object(CopySource=record_dict['CopySource'], Bucket=record_dict['dest_bucket'], Key=record_dict['file_name'])
+    return {
+       'statusCode': 200,
+       'body': json.dumps('File copied to bucket'+dest_bucket)
+    }
